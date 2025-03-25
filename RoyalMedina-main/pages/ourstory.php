d51e1644f53013a03321252d93460d48fc707568
marwa <maramdjj23@gmail.com>
2025-03-25 11:39:32 +0000

    Initial commit

---
+++ RoyalMedina-main/pages/ourstory.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aboutus</title>
+
+    <!--CSS FILE-->
+    <link rel="stylesheet" href="/assets/css/ourstory.css"><!-- Custom CSS file for our story section -->
+    <link rel="stylesheet" href="/assets/css/style.css"><!-- Custom CSS file for our story section -->
+
+    <!--CSS FILE FOR FONT AWESOME ICON -->
+    <!-- Links for font awesome and boxicons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+   <!-- Slick carousel CSS files -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css" />
+
+   <!-- Swiper CSS file -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+</head>
+<body>
+    
+    
+    <!-- Delivery message -->
+        <div class="delivery-message" id="deliveryMessage">
+            <p>FREE LIVRAISON IN MOROCCO OVER 1500 DH</p>
+            <span id="closeIcon" class="close-icon">X</span>
+        </div>
+        
+        <!--navigation -->
+        <nav>
+        <header>
+            <a href="/pages/index.php" class="logo">ROYAL MEDINA </a><!-- Logo and home link -->
+            <ul class="navmenu">
+            <!-- Dropdown menu for COLLECTIONS -->
+                <li><a href="#">COLLECTIONS</a>
+                    <ul class="submenu">
+                        <li><a href="#">CAFTAN</a></li>
+                        <li><a href="#">JEWELRY</a></li>
+                        <li><a href="#">SHOES</a></li>
+                    </ul>
+                </li>
+
+                <li><a href="/pages/ourstory.php">STORY</a></li><!-- Link to our story section -->
+               <!-- Dropdown menu for CONTACT -->
+                <li><a href="#">CONTACT</a>
+                    <ul class="submenu">
+                        <li><a href="/pages/customercare.php">CUSTOMER CARE</a></li>
+                        <li><a href="#">RETURNS</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">BLOG</a></li><!-- Link to the blog -->
+            </ul>
+ <!-- Navigation icons -->
+            <div class="nav-icon">
+                <a href="#"><i class='bx bx-search'></i></a><!-- Search icon -->
+                <a href="#"><i class='bx bx-user-circle' ></i></a><!-- User icon -->
+                <a href="#"><i class='bx bx-cart' ></i></a><!-- Cart icon -->
+                <div class="bx bx-menu" id="menu-icon"></div><!-- Menu icon -->
+            </div>
+        </header>
+    </nav>
+<!-- Section for displaying team members -->
+<section class="team-section">
+    <div class="container">
+        <h2>OUR TEAM</h2>
+        <!-- Team member 1 -->
+        <div class="team-members">
+            <div class="team-member">
+                <div class="member-photo effect">
+                    <img src="/assets/images/ourstory/hananemazyan1.jpg" alt="Membre 1">
+                </div>
+                <h3>  Hanane Mazyan</h3>
+                <p> Junior Full Stack Developer </p>
+            </div>
+            <!-- Team member 2 -->
+            <div class="team-member">
+                <div class="member-photo effect">
+                    <img src="/assets/images/ourstory/marwajaida1.jpg" alt="Membre 2">
+                </div>
+                <h3>Marwa Jaida</h3>
+                <p>Junior Full Stack Developer</p>
+            </div>
+        </div>
+    </div>
+</section>
+ <!-- Section for displaying the project description -->
+<section class="project-section">
+    <div class="container">
+        <h2>OUR STORY</h2>
+        <div class="project-description">
+        <!-- Description of the project -->
+            <p>RoyalMedina is much more than an online store for Moroccan caftans, accessories, and shoes. It is the result of the shared passion of two undergraduate students, Marwa Jaida and Hanane Mazyane, who have combined their creative minds and love for their native country, Morocco.
+
+                Our project emerged from our common desire to promote and celebrate the rich Moroccan culture through its traditional clothing, unique accessories, and beautiful shoes. Each item in our store is carefully selected to represent traditional craftsmanship, authenticity, and the splendor of our cultural heritage.
+                
+                At RoyalMedina, we strive to merge the traditional with the contemporary, offering pieces that embody timeless elegance while adapting to modern trends. Each caftan, accessory, and pair of shoes tells a story, uniting ancestral craftsmanship with a modern and dynamic vision.
+                
+                Our commitment to local craftsmanship and Moroccan artisans is reflected in close partnerships with talented craftsmen, ensuring outstanding quality products made with passion and expertise.
+                
+                Join us in this adventure where beauty, history, and culture converge. With RoyalMedina, immerse yourself in a world where tradition seamlessly blends with modernity to provide a unique and enchanting experience of Moroccan fashion.</p>
+            <img src="/assets/images/logo_try/RoyalMedina1.png" alt="Notre Société">
+        </div>
+    </div>
+</section>
+<!-- Section for customer reviews -->
+<div class="wrapper">
+    <h1>Customer Reviews</h1>
+    <h5> Check what they say about us...</h5>
+    <!--Reviews cards-->
+    <div class="cards">
+        <div class="card">
+            <img src="/assets/images/ourstory/Sandra Buloc.jpg">
+            <p>I recently bought a stunning caftan, exquisite jewelry, and elegant shoes from Royal Medina, and I couldn't be happier! The attention to detail in the craftsmanship is exceptional. The pieces perfectly blend traditional Moroccan beauty with a modern touch. Highly satisfied with my purchase!</p>
+            <h2>Sandra Buloc</h2>
+            <h4>Finance Adisor</h4>
+            <i class="fa fa-quote-left"></i>
+        </div>
+         <!-- Review card 2 -->
+        <div class="card">
+            <img src="/assets/images/ourstory/sofia luck.jpg">
+            <p>Royal Medina is my go-to for authentic Moroccan fashion. The caftans are a true work of art, the jewelry is unique and eye-catching, and the shoes are both stylish and comfortable. The quality is outstanding, and I always receive compliments whenever I wear their pieces. A proud and satisfied customer!</p>
+            <h2>Sophia luck</h2>
+            <h4>Account Professional</h4>
+            <i class="fa fa-quote-left"></i>
+        </div>
+         <!-- Review card 3 -->
+        <div class="card">
+            <img src="/assets/images/ourstory/maria messy.jpg">
+            <p>Royal Medina is my go-to for authentic Moroccan fashion. The caftans are a true work of art, the jewelry is unique and eye-catching, and the shoes are both stylish and comfortable. The quality is outstanding, and I always receive compliments whenever I wear their pieces. A proud and satisfied customer!</p>
+            <h2>Maria Messy</h2>
+            <h4>Restaurent Owner</h4>
+            <i class="fa fa-quote-left"></i>
+        </div>
+    </div>
+</div>
+
+    <!-- footer -->
+    <div class="footer">
+    <!-- Column 1 with useful links -->
+        <div class="col-1">
+            <h3>USEFUL LINKS</h3>
+            <a href="#">Collections</a>
+            <a href="#">Story</a>
+            <a href="#">Contact</a>
+            <a href="#">Blog</a>
+        </div>
+        <!-- Column 2 for getting in touch and newsletter -->
+        <div class="col-2">
+            <h3>GET IN TOUCH AND JOIN IN OUR NEWSLETTER!</h3>
+            <!-- ... (Form to subscribe to the newsletter) ... -->
+            <form>
+                <input type="email" placeholder="Your Email Address" required>
+                <br>
+                <div class="checkboxes">
+                    <label>
+                        <input type="checkbox" id="marketingCheckbox">
+                        <p>I AUTHORIZE THE PROCESSING OF MY PERSONAL DATA FOR MARKETING, COMMERCIAL, MARKET RESEARCH,
+                            PROFILING, AND ADVERTISING PURPOSES.</p>
+                        <br>
+
+                        <input type="checkbox" id="privacyCheckbox">
+                        <p>I AUTHORIZE THE PROCESSING OF MY PERSONAL DATA IN THE WAY AND FOR THE PURPOSES LISTED IN THE
+                            PRIVACY STATEMENT.</p>
+                    </label>
+                    <button type="submit">SUBSCRIBE NOW</button>
+                </div>
+            </form>
+        </div>
+        <!-- Column 3 with contact information and social icons -->
+        <div class="col-3">
+            <h3>CONTACT</h3>
+            <p>Angle Boulevard Sidi Abderrahmane <br> Casablanca, Maroc</p>
+            <div class="social-icons">
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-solid fa-envelope"></i>
+            </div>
+        </div>
+    </div>
+
+        <!-- Copyright -->
+    <div class="copyright">
+        <p>&copy; 2023 Your Company. All rights reserved.</p>
+    </div>
+<!-- JavaScript file for navigation functionality -->
+<script src="/tests/navbar/navbar.js"></script>
+
+</body>
+</html>
